fa330e8d9582a9b5e3ac56387a96e73e14e0373c
Justin Lee <112908236+jc2lee24@users.noreply.github.com>
2023-07-12 14:44:52 -0700

    Update Runner.java

--- Runner.java
+++ Runner.java
@@ -1,11 +1,17 @@
-//import javax.swing.JFrame;
+import javax.swing.JFrame;
 
 
 public class Runner{
 
 	public static void main(String[] args){
 	
-		dataAnalysis DA = new dataAnalysis();
+		JFrame fr = new JFrame("Prediction Model");
+		Screen sc = new Screen();
+		
+		fr.add(sc);
+		fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		fr.pack();
+		fr.setVisible(true);
 	}
 
 }
